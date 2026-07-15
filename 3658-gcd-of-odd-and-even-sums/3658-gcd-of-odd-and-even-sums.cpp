9efb16d3dcd17627b11154b93d7ae7604d6f155a
Akshita Singh Tyagi <144615735+Aki-max-min@users.noreply.github.com>
2026-07-15 22:57:37 +0530

    Time: 0 ms (100%), Space: 8.5 MB (46.67%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,11 @@
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        int sumOdd = n * n;
+        int sumEven = n * (n + 1);
+        return gcd(sumOdd, sumEven);
+    }
+};
