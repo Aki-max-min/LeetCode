6d452b90d2054a8ca0cef79e3fedf5973260efab
Akshita Singh Tyagi <144615735+Aki-max-min@users.noreply.github.com>
2026-08-18 02:20:51 +0530

    Time: 3 ms (19.62%), Space: 37 MB (14.9%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> sumleft(n, 0);
+        vector<int> sumright(n, 0);
+
+        // Calculate left sums
+        for(int i = 1; i < n; i++) {
+            sumleft[i] = sumleft[i - 1] + nums[i - 1];
+        }
+
+        // Calculate right sums
+        for(int i = n - 2; i >= 0; i--) {
+            sumright[i] = sumright[i + 1] + nums[i + 1];
+        }
+
+        // Check pivot
+        for(int i = 0; i < n; i++) {
+            if(sumleft[i] == sumright[i]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
